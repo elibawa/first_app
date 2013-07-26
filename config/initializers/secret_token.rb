10308bc38068261898bfb58947e6af509b5cc251
kelvin bawa <elibawa@yahoo.com>
2013-07-26 01:11:44 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kelvin::Application.config.secret_token = '6071e9f7ccd344871b4e597b2bcc2355ce3c34086394c20e43310f0e442e5077309830a908ab2d3348cbafd1710d06677515723caf2fc49086dba6825faa8bff'
